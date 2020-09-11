bbe33c087b32db6acfbb2b6898c0b3a41d241386
stcalica <kycalica@gmail.com>
2020-09-10 22:27:32 -0700

    used ANTD and replaced a bulk of the nav bar code

--- src/components/Navigation/ReponsiveLogoNavBar.js
+++ src/components/Navigation/ReponsiveLogoNavBar.js
@@ -1,8 +1,8 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import ReactDOM from 'react-dom';
 import { Link} from 'gatsby'
 import { AnchorLink } from 'gatsby-plugin-anchor-links'
-import {  Badge, Layout  } from 'antd'
+import {  Badge, Layout, Menu  } from 'antd'
 import scrollTo from 'gatsby-plugin-smoothscroll'
 import '../../css/main.css'
 import 'antd/dist/antd.css'
@@ -10,71 +10,34 @@ import 'antd/dist/antd.css'
 const { Header } = Layout;
 
 const LinkComponent = (props) => (
-  <li key={props.indexKey}  className = "nav-item">
-  <AnchorLink className="button-link" to={ props.link }> { props.title } </AnchorLink></li >
+  <AnchorLink to={ props.link }> { props.title } </AnchorLink>
 );
 
 const AnchorLinkComponent = (props) => (
-  <li key={props.indexKey} className = "nav-item">
-  <button className="button-link" onClick={() => scrollTo(`${props.link}`)}>{ props.title}</button>
-  </li>
+  <a onClick={() => scrollTo(`${props.link}`)}>{ props.title}</a>
 );
 
-const BuildNavLink = (props) => {
-  console.log(props)
-  let { link, index, count } = props
-  let contents = link.badge ? <Badge count={count}>{ link.title } </Badge> :  link.title
-  return link.link.includes('#') ? <AnchorLinkComponent key={`nav-${index}`} link={link.link.slice(link.link.search('#'))} title={contents}/> : <LinkComponent  key={`nav-${index}`} link={link.link} title={contents}/>
-
-}
-
 const ReponsiveLogoNavBar = (props) => {
-  const mobileMenu = useRef()
 
-  const displayMenu = () => {
-    mobileMenu.current.style.display = (mobileMenu.current.style.display === "none" || mobileMenu.current.style.display === "") ? "flex" : "none";
+  const NavigationLink = (props) => {
+    console.log(props);
+    let contents = props.badge ? <Badge count={props.count}> { props.link.title } </Badge> : props.link.title
+    console.log(contents);
+    return props.link.link.includes('#') ?
+      <AnchorLinkComponent  link={props.link.link.slice(props.link.link.search('#'))} title={contents}/>
+      : <LinkComponent link={props.link.link} title={contents}/>
   }
 
   return (
-    <Header>
-    <nav>
-      <div className = "nav-bar" >
-      <a className="logo-anchor" href="/"><img className = "header-logo" src = "#" / ></a>
-      <ul className = "nav-list" > {
-        props.links.map((link, index) => (
-          <BuildNavLink link={link} index={index} count={props.count} />
-      ))
-      } </ul>
-      <div className = "nav-mobile-btn"
-      id = "mobile-btn"
-      onClick = {
-        () => {
-          displayMenu()
-        }
-      } >
-        <span className = "mobile-bar" ></span>
-        <span className = "mobile-bar"></span>
-        <span className = "mobile-bar"></span>
-      </div>
-    <
-    ul className = "mobile-nav-menu"
-    id = "mobile-nav-menu"
-    ref = {
-      mobileMenu
-    } > {
-      props.links.map((link, index) => < li key = {
-          `mobile-${index}`
-        }
-        className = "mobile-nav-item" > < AnchorLink to = {
-          link.link
-        } > {
-          link.title
-        } < /AnchorLink></li > )
-    } <
-    /ul> < /
-    div >
-    { props.children }
-    </nav>
+    <Header style={{ position: 'fixed', zIndex: 100, width: '100%' }}>
+    <a href="/"><img className="logo-header" src={props.logo}/></a>
+      <Menu theme="dark" mode="horizontal">
+        { props.links.map(link => (
+            <Menu.Item key={`nav-${link.title}`}>
+              { <NavigationLink link={link} count={props.count} badge={link.badge} /> }
+            </Menu.Item>
+        )) }
+      </Menu>
     </Header>
   )
 }
